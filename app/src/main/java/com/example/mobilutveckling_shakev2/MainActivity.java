ca1cb5e4224eb23519ba1f292e4373c0682ac563
tallner <ctallner@hotmail.com>
2022-10-03 17:19:32 +0200

    Sensor manager added

--- app/src/main/java/com/example/mobilutveckling_shakev2/MainActivity.java
+++ app/src/main/java/com/example/mobilutveckling_shakev2/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.mobilutveckling_shakev2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mySensorManager;
+    Sensor accelerometers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            mySensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        }
+        accelerometers = mySensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mySensorManager.registerListener(this,accelerometers,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
